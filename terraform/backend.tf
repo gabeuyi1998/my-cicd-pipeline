47f9eef703e02d8987ffc6fa6a54feb4dc0ef399
Gabriel <uyi1998@gmail.com>
2024-08-30 11:32:48 -0400

    File-restructure

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "go-cicd-bucket"
-    key            = "terraform.tfstate"
-    region         = "ap-southeast-2"
-    dynamodb_table = "GO-TFstate-table"
-  }
-}
--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ecs_service_url" {
-  value = aws_ecs_service.service.network_configuration[0].assign_public_ip
-}
