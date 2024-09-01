ef201d7b9643991ea13e83bb0874546c0f32a70d
Gabriel <uyi1998@gmail.com>
2024-08-31 20:49:51 -0400

    Add Dockerfile and update Terraform backend

--- TF-Automation/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = var.terraform_state_bucket
-    key            = "dev/terraform.tfstate"
-    region         = "ap-southeast-2"
-    dynamodb_table = var.terraform_locks_table
-  }
-}
--- TF-Automation/dev-tf/backend.tf
+++ TF-Automation/dev-tf/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "go-cicd-bucket"
-    key            = "dev/terraform.tfstate"  # Organized under "dev" directory in the S3 bucket
-    region         = "ap-southeast-2"
-    dynamodb_table = "GO-TFstate-table"
+    bucket         = "go-cicd-bucket"                # S3 bucket name for storing the state file
+    key            = "dev/terraform.tfstate"         # Path to the state file within the S3 bucket (organized under "dev" directory)
+    region         = "ap-southeast-2"                # AWS region where the S3 bucket and DynamoDB table are located
+    dynamodb_table = "GO-TFstate-table"              # DynamoDB table for state locking and consistency
   }
 }
