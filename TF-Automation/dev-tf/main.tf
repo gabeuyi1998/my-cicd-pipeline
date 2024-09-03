ee92ac2580cee99e19106f2fb72e1c9907a1f48a
Gabriel <uyi1998@gmail.com>
2024-09-03 10:57:30 -0400

    main.tf fix v5

--- TF-Automation/dev-tf/main.tf
+++ TF-Automation/dev-tf/main.tf
@@ -18,11 +18,6 @@ module "ecr" {
   create_repo = try(length(data.aws_ecr_repository.existing_repo.id) == 0, true)
 }
 
-output "ecr_repo_url" {
-  description = "The URL of the ECR repository"
-  value       = module.ecr.repo_url
-}
-
 # IAM Module
 data "aws_iam_role" "existing_ecs_role" {
   name = var.ecs_task_execution_role_name
@@ -41,16 +36,6 @@ module "iam" {
   create_ssm_role              = try(length(data.aws_iam_role.existing_ssm_role.id) == 0, true)
 }
 
-output "ecs_task_execution_role_arn" {
-  description = "The ARN of the ECS task execution role"
-  value       = module.iam.ecs_task_execution_role_arn
-}
-
-output "ssm_profile_name" {
-  description = "The name of the SSM instance profile"
-  value       = module.iam.ssm_profile_name
-}
-
 # ECS Module
 module "ecs" {
   source                       = "./modules/ecs"
@@ -63,21 +48,6 @@ module "ecs" {
   security_groups              = var.security_groups
 }
 
-output "ecs_cluster_id" {
-  description = "The ID of the ECS cluster"
-  value       = module.ecs.cluster_id
-}
-
-output "ecs_service_name" {
-  description = "The name of the ECS service"
-  value       = module.ecs.service_name
-}
-
-output "ecs_task_definition_arn" {
-  description = "The ARN of the ECS task definition"
-  value       = module.ecs.task_definition_arn
-}
-
 # EC2 Module
 module "ec2" {
   source                    = "./modules/ec2"
@@ -87,14 +57,4 @@ module "ec2" {
   subnet_id                 = var.subnet_id
   vpc_security_group_ids    = var.vpc_security_group_ids
   iam_instance_profile_name = module.iam.ssm_profile_name
-}
-
-output "ec2_instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = module.ec2.instance_id
-}
-
-output "ec2_public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = module.ec2.public_ip
 }
